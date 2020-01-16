fcbe35e5ddd4047a78fa9638617ff59214de0fc8
Daniel Liberato <danielrcliberato@gmail.com>
2020-01-16 12:39:58 -0200

    Upload folder web

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Estado
+// Propriedade
+
+function App() {
+  return (
+    <Header/>
+  );
+}
+
+export default App;
---
+++ web/src/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+    return <h1> Semana OmniStack 10</h1>
+}
+
+export default Header;
