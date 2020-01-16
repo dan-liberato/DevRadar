644a1bdb6b535737339fd4f1b5e6a17d0870cccf
Daniel Liberato <danielrcliberato@gmail.com>
2020-01-16 17:38:20 -0200

    Update components

--- web/src/App.js
+++ web/src/App.js
@@ -1,131 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api'
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 function App() {
-    const [latitude, setLatitude] = useState('');
-    const [longitude, setLongitude] = useState('');
+    const [devs, setDevs] = useState([]);
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const { latitude, longitude } = position.coords;
+    useEffect( () => {
+        async function loadDevs() {
+            const response = await api.get('/devs');
 
-                setLatitude(latitude);
-                setLongitude(longitude);
-            },
-            (err) => {
-                console.log(err);
-            }, 
-            {
-                timeout: 30000,
-            }
-        );
+            setDevs(response.data);
+        }
+
+        loadDevs();
     }, []);
+
+    async function handleAddDev(data) {
+        const response = await api.post('/devs', data)
+
+        setDevs([...devs, response.data]);
+    }
     
     return (
         <div id="app">
             <aside>
                 <strong>Cadastrar</strong>
-                <form>
-                    <div className="input-block">
-                        <label htmlFor="github_username">Usuário do Github</label>
-                        <input name="github_username" id="github_username" required />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="techs">Tecnologias</label>
-                        <input name="techs" id="techs" required />
-                    </div>
-
-                    <div className="input-group">
-                        <div className="input-block">
-                            <label htmlFor="latitude">Latitude</label>
-                            <input 
-                                type="number" 
-                                name="latitude" 
-                                id="latitude" 
-                                required value={ latitude }
-                                onChange={e => setLatitude(e.target.value)}    
-                            />
-                        </div>
-
-                        <div className="input-block">
-                            <label htmlFor="longitude">Longitude</label>
-                            <input 
-                                type="number" 
-                                name="longitude" 
-                                id="longitude" 
-                                required value={ longitude }
-                                onChange={ e=> setLongitude(e.target.value)}
-                            />
-                        </div>
-                    </div>
-
-                    <button type="submit">Salvar</button>
-                </form>
+                <DevForm onSubmit={ handleAddDev } />
             </aside>
 
             <main>
                 <ul>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars3.githubusercontent.com/u/6700671?s=460&v=4" alt="Daniel Liberato"/>
-                            <div className="user-info">
-                                <strong>Daniel Liberato</strong>
-                                <span>ReactJS, React Native, Node.js, WordPress, PHP</span>
-                            </div>
-                        </header>
-                        <p>
-                            Desenvolvedor de Software e fundador da Ogma. Apaixonado por CSS e UI Design.
-                            <a href="https://github.com/dan-liberato">Acessar perfil no Github</a>
-                        </p>
-                    </li>
 
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars3.githubusercontent.com/u/6700671?s=460&v=4" alt="Daniel Liberato"/>
-                            <div className="user-info">
-                                <strong>Daniel Liberato</strong>
-                                <span>ReactJS, React Native, Node.js, WordPress, PHP</span>
-                            </div>
-                        </header>
-                        <p>
-                            Desenvolvedor de Software e fundador da Ogma. Apaixonado por CSS e UI Design.
-                            <a href="https://github.com/dan-liberato">Acessar perfil no Github</a>
-                        </p>
-                    </li>
+                    {devs.map(dev => (
+                        <DevItem key={dev._id} dev={dev}/>
+                    ))}
 
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars3.githubusercontent.com/u/6700671?s=460&v=4" alt="Daniel Liberato"/>
-                            <div className="user-info">
-                                <strong>Daniel Liberato</strong>
-                                <span>ReactJS, React Native, Node.js, WordPress, PHP</span>
-                            </div>
-                        </header>
-                        <p>
-                            Desenvolvedor de Software e fundador da Ogma. Apaixonado por CSS e UI Design.
-                            <a href="https://github.com/dan-liberato">Acessar perfil no Github</a>
-                        </p>
-                    </li>
-
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars3.githubusercontent.com/u/6700671?s=460&v=4" alt="Daniel Liberato"/>
-                            <div className="user-info">
-                                <strong>Daniel Liberato</strong>
-                                <span>ReactJS, React Native, Node.js, WordPress, PHP</span>
-                            </div>
-                        </header>
-                        <p>
-                            Desenvolvedor de Software e fundador da Ogma. Apaixonado por CSS e UI Design.
-                            <a href="https://github.com/dan-liberato">Acessar perfil no Github</a>
-                        </p>
-                    </li>
                 </ul>
             </main>
         </div>
