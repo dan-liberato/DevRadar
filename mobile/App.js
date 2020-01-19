6ee44b07bcdf766eb09ce0d578b6c4ddc6faac1b
Daniel Liberato <danielrcliberato@gmail.com>
2020-01-19 19:53:40 -0300

    Update

--- mobile/App.js
+++ mobile/App.js
@@ -1,51 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-
-      <View style={styles.box}>
-        <Text>Box</Text>
-      </View>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7"/>
+      <Routes />
+    </>
   );
 }
-
-// Box shadow
-function elevationShadowStyle(elevation) {
-  return {
-    elevation,
-    shadowColor: 'black',
-    shadowOffset: { width: 0.5, height: 0.5 * elevation },
-    shadowOpacity: 0.3,
-    shadowRadius: 0.8 * elevation
-  };
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 32,
-    color: '#fff',
-  },
-  box: {
-    // Add Shadow
-    ...elevationShadowStyle(5),
-    width: 100,
-    height: 100,
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 5,
-    marginTop: 30,
-  }
-});
